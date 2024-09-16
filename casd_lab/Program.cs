947dd48b86db3d762f749bf0b9f145afc866f55f
deka4core <my.win10.home@outlook.com>
2024-09-17 01:22:29 +0300

    test commit

---
+++ casd_lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace casd_lab
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
