9b4b34beae7009b62cbb5e2540e0c63883dd9a9c
deka4core <my.win10.home@outlook.com>
2024-09-18 11:27:51 +0300

    Задача 2. Добавлен неполный класс комплексного числа

--- casd_lab/Program.cs
+++ casd_lab/Program.cs
@@ -4,9 +4,18 @@ namespace casd_lab
 {
     internal class Program
     {
+        class Complex
+        {
+            private double real;
+            private double imaginary;
+            public Complex(double real, double imaginary)
+            {
+                this.imaginary = imaginary;
+            }
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
         }
     }
 }
