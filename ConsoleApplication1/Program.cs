23471dac61f92aedad7134fa7dab15f72ae397df
deka4core <94462798+deka4core@users.noreply.github.com>
2024-11-08 19:37:50 +0300

    Update Program.cs
    final final no testing lab 4

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,24 +6,6 @@ namespace ConsoleApplication1
     {
         public static void Main(string[] args)
         {
-            // Тестирование
-            var list = new MyArrayList<int>();
-            for (var i = 1; i <= 10; i++)
-            {
-                list.Add(i);
-            }
-            
-            Console.WriteLine(list.Get(4)); 
-            // Console.WriteLine(list.Get(150));
-            list.Remove(4);
-            Console.WriteLine(list.Size());
-            Console.WriteLine(list.IsEmpty());
-            list.RemoveAll(new int[] { 2, 3 });
-            var sublist = list.SubList(1, 5);
-            list.RetainAll(new int[] { 1 });
-            Console.WriteLine(list.Size());
-            Console.WriteLine(list.Get(0));
-            return;
         }
     }
 }
